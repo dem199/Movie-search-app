e99f4310ddd84e9626c9688c3bb51aac5052aea0
Opeyemi Olatunbosun <olatunbosunopeyemi186035@gmail.com>
2023-05-06 03:10:08 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import SearchMovies from "./SearchMovies.jsx";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">React Movie Search</h1>
+      <SearchMovies />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from "./App.jsx";
+import "regenerator-runtime/runtime";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
